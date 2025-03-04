1d058847eaba9af0c2673b64e0934fe36a55f025
RamDev <daudalaramadan2@gmail.com>
2025-03-04 11:06:41 +0000

    ✨ Update ArrayOfObjects component; change id values to numbers and convert to strings for keyExtractor

--- src/components/ArrayOfObjects.tsx
+++ src/components/ArrayOfObjects.tsx
@@ -3,18 +3,19 @@ import React from "react";
 
 const ArrayOfObjects = () => {
   const anArray = [
-    { id: "2333-1", name: "kofi" },
-    { id: "2333-2", name: "kwame" },
-    { id: "2333-3", name: "ama" },
-    { id: "2333-4", name: "yaw" },
+    { id: 23331, name: "kofi" },
+    { id: 23332, name: "kwame" },
+    { id: 23333, name: "ama" },
+    { id: 23334, name: "yaw" },
   ];
 
   return (
+    // note that these are not strings but numbers so we hv to convert them to stings
     <View>
       <Text>ArrayOfObjects</Text>
       <FlatList
         data={anArray}
-        keyExtractor={(item) => item.id}
+        keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => <Text>{item.name}</Text>}
       />
     </View>
